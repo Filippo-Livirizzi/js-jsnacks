6924bb17dd0ccce8f46630afc15d7bb9d2bfcae1
Filippo-Livirizzi <livirizzifilippo@gmail.com>
2024-03-20 15:59:57 +0100

    add: finito lo snack numero 3, aggiungendo un secondo ciclo for

--- js/js3/app.js
+++ js/js3/app.js
@@ -1,9 +1,15 @@
 //JSnack 3
 
 let numeri = []; //creo array
+let sum = 0;
 //apro un ciclo for
 for (let j = 0; j < 10; j++) {
     numeri.push(prompt("Inserire un numero:"));
     console.log(numeri);
 }
-//fare la somma dei numeri
+for (let i =0; i<numeri.length; i++){
+    const num = numeri[i];
+    console.log(num);
+    sum += num;
+}
+console.log(sum);
